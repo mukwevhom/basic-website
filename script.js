3c337fe506b338e5e2673e78048ea5e0be6a2db2
Murendeni Mukwevho <murendenim@girlcode.co.za>
2024-05-28 14:25:00 +0200

    Removed the button from about section

--- script.js
+++ script.js
@@ -8,16 +8,9 @@ let aboutSectionHTML = `<div class="container">
         <h2>A little about me</h2>
         <p>Thanks for stopping by. For the past 6 years, I've excelled in customer success teams at some of Australia's top hotels. Recently, I joined the customer success team at Square, where I'm proud to be part of a team building a more inclusive economy for small businesses in Australia.</p>
         <p>I have an unwavering passion and energy for people, which has proven a valuable asset in my career. I believe the best customer experiences come from genuine empathy, while always maintaining a friendly, positive and energetic character.</p>
-        <button>Click Me</button>
     </div>
 </div>`
 
 aboutSection.innerHTML = aboutSectionHTML
-
-let newButton = aboutSection.querySelector("button")
-
-newButton.addEventListener("click", () => {
-    console.log("New Button Clicked")
-})
 
 introSection.after(aboutSection)

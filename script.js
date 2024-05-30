d16500ee8c4645b507728bd6ba1dd7aab365d881
Murendeni Mukwevho <murendenim@girlcode.co.za>
2024-05-30 14:49:15 +0200

    Adding Work History

--- script.js
+++ script.js
@@ -16,3 +16,48 @@ aboutSection.innerHTML = aboutSectionHTML
 introSection.after(aboutSection)
 
 document.querySelector(".footer-year").textContent = new Date().getFullYear()
+
+// List Work History
+const workHistoryArr = [
+    {
+        "image": "pexels-olly-846741.jpg",
+        "period": "August 2021 - Current",
+        "company": "Square",
+        "about": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur doloremque assumenda, quis quae corporis aut est ducimus in commodi soluta dolore nam ipsam mollitia accusantium similique, dolorem ipsum quos architecto?"
+    },
+    {
+        "image": "pexels-seven11nash-380769.jpg",
+        "period": "March 2021 - August 2021",
+        "company": "Hilton Melbourne Little Queen Street",
+        "about": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur doloremque assumenda, quis quae corporis aut est ducimus in commodi soluta dolore nam ipsam mollitia accusantium similique, dolorem ipsum quos architecto?"
+    },
+    {
+        "image": "pexels-seven11nash-380769.jpg",
+        "period": "February 2018 - March 2021",
+        "company": "West Hotel Sydney, Curio Collection by Hilton",
+        "about": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur doloremque assumenda, quis quae corporis aut est ducimus in commodi soluta dolore nam ipsam mollitia accusantium similique, dolorem ipsum quos architecto?"
+    }
+]
+
+let workHistoryContEl = document.querySelector(".work-history-section .container")
+
+workHistoryArr.forEach(item => {
+    let workHTML = `<div class="row align-items-start py-5">
+        <div class="col">
+            <img src="${item.image}" alt="Photo by Marc Mueller: https://www.pexels.com/photo/man-sitting-in-front-of-computer-380769/" class="w-100" >
+        </div>
+        <div class="col">
+            <div class="work-info">
+                <p>${item.period}</p>
+                <h3>${item.company}</h3>
+                <p>${item.about}</p>
+            </div>
+        </div>
+    </div>`
+
+    let tempWorkDivEl = document.createElement("div")
+    
+    tempWorkDivEl.innerHTML = workHTML
+
+    workHistoryContEl.append(tempWorkDivEl)
+})
